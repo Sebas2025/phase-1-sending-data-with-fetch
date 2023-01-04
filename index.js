5c1277dad0fa495631b8eacb1618ba2d1af39178
SebastainBernal <sebasair2025@gmail.com>
2023-01-03 20:15:44 -0700

    commit tree 77b99dccc5a89ee6570632ddd310ac69c488d7bf

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your code here
+// Needs a function submitData(usersname,useremail)
+
+function submitData(username,useremail){
+       const configurationObject = {
+        headers:{
+            "Content-Type":"application/json",
+            "Accept":"application/json"
+        },
+        body: JSON.stringify({
+            username: "Steve",
+            useremail:"steve@steve.com"
+        })
+       }
+}
+
+fetch(`http://localhost:3000/users`,configurationObject);
